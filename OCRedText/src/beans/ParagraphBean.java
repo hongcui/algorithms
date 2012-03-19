240d24adfaaaf0232ae454bbe441c6477f5c4a4c
huangfengq <huangfengq@gmail.com>
2012-03-19 20:48:29 +0000

    vol b, o passed

---
+++ OCRedText/src/beans/ParagraphBean.java
@@ -0,0 +1,41 @@
+package beans;
+
+public class ParagraphBean {
+	private int originalID;
+	private String para;
+	private String source;
+
+	public void normalize() {
+		para.replaceAll("(?<=[a-z])-\\s+(?=[a-z])", "-")
+				.replaceAll("\\s+", " ").replaceAll("\\^", "").replaceAll("\\“", "\"").replaceAll("\\”", "\"");
+	}
+
+	public ParagraphBean(String para, int originalID) {
+		this.para = para;
+		this.originalID = originalID;
+	}
+
+	public String getPara() {
+		return para;
+	}
+
+	public void setPara(String para) {
+		this.para = para;
+	}
+
+	public int getOriginalID() {
+		return originalID;
+	}
+
+	public void setOriginalID(int originalID) {
+		this.originalID = originalID;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+}
